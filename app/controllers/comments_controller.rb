3b89f05646d9855d014bf7c77f507bfc00afffb2
07naoki07 <naokiti.0205.22@gmail.com>
2022-03-14 14:00:14 +0000

    review comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+    
+ def create
+  book = Book.find(params[:book_id])
+  comment =current_user.comments.new(comment_params)
+  comment.book_id =book.id
+  comment.save
+  redirect_to request.referer
+ end
+ 
+ private
+  def comment_params
+   params.require(:comment).pemit(:comment)
+  end
+  
 end
