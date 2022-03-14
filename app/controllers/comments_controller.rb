2f87a2eb5ef36507ff969063ddaf91ecf1a054d9
07naoki07 <naokiti.0205.22@gmail.com>
2022-03-14 15:38:07 +0000

    review comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,23 @@
 class CommentsController < ApplicationController
     
  def create
-  book = Book.find(params[:book_id])
-  comment =current_user.comments.new(comment_params)
-  comment.book_id =book.id
-  comment.save
+  @book = Book.find(params[:book_id])
+  @comment =current_user.comments.new(comment_params)
+  @comment.book_id =@book.id
+  @comment.save
+  redirect_to request.referer
+ end
+ 
+ def destroy
+  Comment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+  flash.now[:alert] = '投稿を削除しました'
+  @book = Book.find(params[:book_id])  
   redirect_to request.referer
  end
  
  private
   def comment_params
-   params.require(:comment).pemit(:comment)
+   params.require(:comment).permit(:comment)
   end
   
 end
