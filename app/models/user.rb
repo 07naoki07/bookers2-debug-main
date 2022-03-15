462e3a2c8b5ed03cfbff150e29dfb6b9f6ec2f33
07naoki07 <naokiti.0205.22@gmail.com>
2022-03-15 10:05:45 +0000

    review 4task

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,14 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites,dependent: :destroy
   has_many :comments, dependent: :destroy
+  
+  has_many :relationships,class_name:"Relationship",foreign_key:"follwer_id",dependent: :destroy
+  has_many :reverse_of_relationships,class_name:"Relationship",foreign_key:"follwed_id",dependent: :destroy
 
+  has_many :followers, through: :relationship,source: :follower
+  has_many :followeds, through: :reverse_of_relationships,source: :followed
+  
   has_one_attached :profile_image
- 
   
   validates :name, uniqueness: true,presence: true,length: { minimum: 2, maximum: 20 }
   validates :introduction,length: { maximum: 50 }
